87ffda096c831861ad7d8968fde98c40e668bcdc
sudhansu <sudhansu@convertcart.com>
2023-08-09 18:50:08 +0530

    added-test

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,7 +1,17 @@
+/**
+ * Adds a leading zero to single digit numbers.
+ * @param {number} n - The number to prepend with a zero if necessary.
+ * @returns {string} - The number with a leading zero if necessary.
+ */
 function prependToSingleDigit(n) {
-  return n > 9 ? "" + n : "0" + n;
+  return n > 9 ? String(n) : "0" + n;
 }
 
+/**
+ * Converts tenths of a second to an object representing hours, minutes, seconds, and milliseconds.
+ * @param {number} onTenthSecond - The value representing tenths of a second.
+ * @returns {object} - The time object with hours, minutes, seconds, and milliseconds.
+ */
 function getTimeObject(onTenthSecond) {
   let milliseconds = prependToSingleDigit(onTenthSecond % 100);
   let remaining = (onTenthSecond - milliseconds) / 100;
@@ -10,15 +20,24 @@ function getTimeObject(onTenthSecond) {
   let mins = prependToSingleDigit(remaining % 60);
   remaining = (remaining - mins) / 60;
   let hrs = prependToSingleDigit(remaining % 60);
-  remaining = (remaining - hrs) / 60;
   //   You can add days months years to extend this functionality
   return { hrs, mins, seconds, milliseconds };
 }
 
+/**
+ * Hides the specified elements by setting their display style property to "none".
+ * @param {Array} elements - The elements to be hidden.
+ */
 function removeElements(elements) {
   elements.forEach((element) => (element.style.display = "none"));
 }
+
+/**
+ * Shows the specified elements by setting their display style property to "block".
+ * @param {Array} elements - The elements to be shown.
+ */
 function addElements(elements) {
   elements.forEach((element) => (element.style.display = "block"));
 }
+
 export { getTimeObject, removeElements, addElements };
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,37 @@
 import { getTimeObject, removeElements, addElements } from "./helper.js";
+/**
+ * The 'code_under_test' code snippet defines a class called 'Stopwatch' that represents a stopwatch functionality.
+ * It provides features like start, stop, resume, reset, and lap.
+ * The stopwatch uses helper functions from the 'helper.js' file to manipulate the DOM and calculate time.
+ *
+ * Example Usage:
+ * const stopWatch = new Stopwatch();
+ *
+ * The Stopwatch class has the following properties and methods:
+ *
+ * Properties:
+ * - currentLap: An array to store lap data.
+ * - millisecondsPassed: A variable to keep track of the milliseconds passed.
+ * - timerRef: A reference to the setInterval timer.
+ *
+ * Methods:
+ * - startTimer: Starts the timer by incrementing 'millisecondsPassed' every 10 milliseconds.
+ *               It also updates the timer display on the webpage.
+ * - stopTimer: Stops the timer by clearing the interval timer.
+ * - addLap: Adds the current lap data (in the format of { hrs, mins, seconds, milliseconds }) to the 'currentLap' array.
+ * - resetTimer: Resets the timer by setting 'millisecondsPassed' to 0, updating the timer display to "00:00.00",
+ *               and clearing the 'currentLap' array.
+ * - getLaps: Returns the 'currentLap' array.
+ *
+ * The code snippet also defines event listeners for buttons on the webpage:
+ * - startButton: Starts the timer when clicked, hides the start button, and shows the stop and lap buttons.
+ * - stopButton: Stops the timer when clicked, hides the stop and lap buttons, and shows the resume and reset buttons.
+ * - resumeButton: Resumes the timer when clicked, hides the resume and reset buttons, and shows the stop and lap buttons.
+ * - resetButton: Resets the timer when clicked, hides the resume and reset buttons, shows the start button, and clears the lap container.
+ * - lapBtn: Adds a lap when clicked, gets the lap data from the stopwatch, creates a new lap row element, and appends it to the lap container.
+ *
+ * Overall, the code snippet provides a functional stopwatch with lap functionality on a webpage.
+ */
 
 function Stopwatch() {
   let currentLap = [];
@@ -52,11 +85,17 @@ const stopButton = document.querySelector(".stop-watch__btn--stop");
 const resumeButton = document.querySelector(".stop-watch__btn--resume");
 const resetButton = document.querySelector(".stop-watch__btn--reset");
 
-function getLapRow(clockData) {
-  const trElement = document.createElement("p");
+/**
+ * Creates a new lap row element with the given clock data.
+ * @param {Object} clockData - The clock data object containing minutes, seconds, and milliseconds.
+ * @param {number} lapId - The ID of the lap.
+ * @returns {HTMLElement} - The lap row element.
+ */
+function getLapRow(clockData, lapId) {
+  const lapRowElement = document.createElement("p");
   const { mins, seconds, milliseconds } = clockData;
-  trElement.innerText = `${mins}:${seconds}.${milliseconds}`;
-  return trElement;
+  lapRowElement.innerText = `Lap ${lapId}  \xa0\xa0\xa0\xa0\xa0\xa0\xa0 ${mins}:${seconds}.${milliseconds}\xa0\xa0\xa0\xa0\xa0\xa0\xa0`;
+  return lapRowElement;
 }
 
 startButton.addEventListener("click", function () {
@@ -86,9 +125,10 @@ resetButton.addEventListener("click", function () {
 
 lapBtn.addEventListener("click", function () {
   stopWatch.addLap;
-  console.log(lapContainer);
   const lapData = stopWatch.getLaps;
-  const lastElement = [...lapData].pop();
-  const rowElment = getLapRow(lastElement);
+  const lastElement = lapData[lapData.length - 1];
+  const secondLastElement = lapData[lapData.lenght - 2];
+  const rowElment = getLapRow(lastElement, lapData.length);
   lapContainer.appendChild(rowElment);
+  lapContainer.scrollTop = lapContainer.scrollHeight;
 });
